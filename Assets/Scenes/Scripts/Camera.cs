9cb529bebdc16db6b26b59757d984e530902f51a
Damian <jei.grave@gmail.com>
2025-01-26 16:10:30 +0800

    Added Camera Script
    Camera now follows the player for an offset of 0.13f

---
+++ Assets/Scenes/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float smoothSpeed = 0.13f;
+
+    void LateUpdate(){
+        if (player != null){
+            Vector3 desiredPosition = player.position + offset;
+            Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+            transform.position = smoothPosition;
+        }
+    }
+
+}
